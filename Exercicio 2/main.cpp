858d799104ed96a2f053c7822a9753740d742c0c
LynconLy <125322338+LynconLy@users.noreply.github.com>
2023-03-24 19:47:05 -0300

    Update main.cpp

--- Exercicio 2/main.cpp
+++ Exercicio 2/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <locale>
 
 using namespace std;
 
-int main()
+int main() 
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL,"PORTUGUESE");
+    
+    int numAlunos = 20;
+    float notas[numAlunos];
+    float media = 0;
+    int countAcimaMedia = 0;
+    
+    for (int i = 0; i < numAlunos; i++) 
+    {
+        cout << "Digite a nota do aluno " << i + 1 << ": ";
+        cin >> notas[i];
+        media += notas[i];
+    }
+    
+    media /= numAlunos;
+    
+    for (int i = 0; i < numAlunos; i++)
+    {
+        if (notas[i] > media) 
+        {
+            countAcimaMedia++;
+        }
+    }
+    
+    cout << "Média da turma: " << media << endl;
+    cout << "Número de alunos com nota acima da média: " << countAcimaMedia << endl;
+    
     return 0;
 }
