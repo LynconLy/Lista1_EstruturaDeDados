24afa869bc460c2c5d493927c4ad0fcab28450b6
LynconLy <125322338+LynconLy@users.noreply.github.com>
2023-03-24 19:50:19 -0300

    Update main.cpp

--- Exercicio 6 - ED/main.cpp
+++ Exercicio 6 - ED/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <locale>
 
 using namespace std;
 
-int main()
+int main() 
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL, "PORTUGUESE");
+    
+    int diasAno = 365;
+    float tempMed[diasAno];
+    float menorTemp = 9999;
+    float maiorTemp = -9999;
+    float tempMedia = 0;
+    int countInfMedia = 0;
+    
+    for (int i = 0; i < diasAno; i++) 
+    {
+        cout << "Digite a temperatura média do dia " << i+1 << ": ";
+        cin >> tempMed[i];
+        
+        if (tempMed[i] < menorTemp) 
+        {
+            menorTemp = tempMed[i];
+        }
+        
+        if (tempMed[i] > maiorTemp)
+        {
+            maiorTemp = tempMed[i];
+        }
+        
+        tempMedia += tempMed[i];
+    }
+    
+    tempMedia /= diasAno;
+    
+    for (int i = 0; i < diasAno; i++)
+    {
+        if (tempMed[i] < tempMedia)
+        {
+            countInfMedia++;
+        }
+    }
+    
+    cout << "Menor temperatura do ano: " << menorTemp << endl;
+    cout << "Maior temperatura do ano: " << maiorTemp << endl;
+    cout << "Temperatura média anual: " << tempMedia << endl;
+    cout << "Número de dias no ano em que a temperatura foi inferior à média anual: " << countInfMedia << endl;
+    
     return 0;
 }
